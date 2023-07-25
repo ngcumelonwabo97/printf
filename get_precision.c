0a16c5a6c94baacf41c78837153c900ec63fa8a7
Lonwabo <ngcumelonwabo97@gmail.com>
2023-07-25 10:07:23 -0700

    remaining files

--- get_precision.c
+++ get_precision.c
@@ -2,12 +2,12 @@
 
 /**
  * get_precision - Calculates the precision for printing
- * @format: string in which to print the arguments
- * @i: list of arguments to be printed
- * @list: list of arguments
- * Return: Precision
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
  */
-
 int get_precision(const char *format, int *i, va_list list)
 {
 	int curr_i = *i + 1;
